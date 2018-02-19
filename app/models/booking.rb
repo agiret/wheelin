e82c1c249018d97affa04bc48553ccb5857f4558
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-02-19 18:43:19 +0100

    Validations des models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :wheely
   belongs_to :user
+
+  validates :booking_day, prensence: true
+  validates :wheely_id, prensence: true
+  validates_uniqueness_of :booking_day, scope: [:wheely_id]
 end
--- app/models/wheely.rb
+++ app/models/wheely.rb
@@ -2,4 +2,8 @@ class Wheely < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many :bookings
+
+  validates :address, prensence: true
+  validates :description, prensence: true
+  validates :category_id, prensence: true
 end
