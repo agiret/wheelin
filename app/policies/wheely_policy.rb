28255413f1410cd001ebf110e8f935a0be55892f
Sebastien Balas <balas.sebastien@gmail.com>
2018-02-20 18:09:11 +0100

    pumdit init

---
+++ app/policies/wheely_policy.rb
@@ -0,0 +1,21 @@
+class WheelyPolicy < ApplicationPolicy
+  def create?
+    return true
+  end
+
+  def update?
+    record.user == user
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
