16286e2cae919fa9b024066678a0d9f0a157e8e3
Maxime Personnic <maxime.personnic@hotmail.fr>
2018-02-20 11:43:12 +0100

    add BookingController and view Bookings#new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,22 @@
 class BookingsController < ApplicationController
   def new
+    @wheely = Wheely.find(params[:wheely_id])
+    @booking = Booking.new
   end
 
   def create
+    @wheely = Wheely.find(params[:wheely_id])
+    @booking = @wheely.booking.new(booking_params)
+    if @booking.save
+      redirect_to wheely_path(@wheely)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:booking_day, :wheely_id)
   end
 end
