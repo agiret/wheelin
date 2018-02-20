200152dbebb0e3b3bbccef9e4da513f761475a0f
Maxime Personnic <maxime.personnic@hotmail.fr>
2018-02-20 12:24:05 +0100

    waiting modif typo presence to continue

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Category.destroy_all
+Wheely.destroy_all
+
+CATEGORIES = ["Vélo", "Skateboard", "Roller", "Trotinette", "Monocycle", "tandem"]
+
+CATEGORIES.each do |category|
+  Category.create(name: category)
+end
