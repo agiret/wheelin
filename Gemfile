fb0312557851211f20579ae92b6e8423f0c557d5
Maxime Personnic <maxime.personnic@hotmail.fr>
2018-02-21 14:40:48 +0100

    add uploader image

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'devise'
 gem 'faker'
 gem "pundit"
 gem "geocoder"
+gem 'cloudinary'
+gem 'carrierwave', '~> 1.2'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/models/wheely.rb
+++ app/models/wheely.rb
@@ -9,4 +9,7 @@ class Wheely < ApplicationRecord
   validates :user, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  mount_uploader :photo, PhotoUploader
+
 end
